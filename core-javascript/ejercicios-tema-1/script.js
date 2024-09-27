c7754f3c6c086b17ddc69fc5c63cb4ac96993c90
Iñaki <inaki.barcelo@gmail.com>
2024-09-27 13:02:48 +0200

    Ejercicios tema 1 completos

---
+++ core-javascript/ejercicios-tema-1/script.js
@@ -0,0 +1,16 @@
+// 1. Declara una variable normal y una constante
+let mensaje = "Hola, mundo!";
+const PI = 3.14159;
+//2. Declara una variable normal. Asígnala después a un número
+let numero;
+numero = 6;
+//3. Declara dos variables numéricas y declara una tercera con su suma
+let numeroUno = 5;
+let numeroDos = 8;
+let suma = numeroUno + numeroDos;
+console.log(suma);
+//4. Declara dos variables de string y declara una tercera con su suma
+let palabraUno = "Hola";
+let palabraDos = " Mundo!";
+let concatena = palabraUno + palabraDos;
+console.log(concatena);
