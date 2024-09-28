a3ecdd65ac962ce3987811d55aa9265a1c500446
Iñaki <inaki.barcelo@gmail.com>
2024-09-28 10:03:54 +0200

    Ejercicio 4 Tema 3 con validaciones

--- core-javascript/ejercicios-tema-3/scriptWheels.js
+++ core-javascript/ejercicios-tema-3/scriptWheels.js
@@ -7,10 +7,25 @@
 //deberá mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado”
 //Ejecuta el código y comprueba sus resultados. Para comprobar si es correcta tu
 //solución puedes consultar en los foros aprenderaprogramar.com.
+
 let diametro = prompt("¿Qué diámetro tiene la rueda?");
 let grosor = prompt("¿Qué grosor tiene la rueda?");
 
 const calcularTipoVehiculo = () => {
+    //Convertir a número
+    diametro = parseFloat(diametro);
+    grosor = parseFloat(grosor);
+
+    //Validaciones 
+    if (isNaN(diametro) || isNaN(grosor)) {
+        alert("Por favor, introduce solo números.");
+        return;
+    }
+    if (diametro <= 0 || grosor <= 0) {
+        alert("Por favor, introduce solo números positivos.");
+        return;
+    }
+
     if ((diametro>1.4 && grosor<0.4) ||(diametro<=1.4 && diametro>0.8 && grosor<0.25)) {
         alert("El grosor para esta rueda es inferior al recomendado");
     } else if (diametro>1.4){
