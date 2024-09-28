dfdbc8c1209f01bbfde0c681cc5b4f7b99f4ae37
Iñaki <inaki.barcelo@gmail.com>
2024-09-28 13:03:51 +0200

    Ejercicios 1, 2 y 3 resuletos del Tema 4, me voy a Alsasua

---
+++ core-javascript/ejercicios-tema-4/script.js
@@ -0,0 +1,19 @@
+//1.1. Imprimir los números del 1 al 100 de uno en uno como alerta
+for (let i=1; i<=100; i++){
+    //alert(i);
+    console.log(i);//La alerta es un peñazo, lo cambio a console.log
+};
+//1.2. Imprimir sólo los números pares del 1 al 100 en log de consola
+for (let i=1; i<=100; i++){
+    if (i % 2 ===0) {
+    console.log("Par " + i);//La alerta es un peñazo, lo cambio a console.log
+    }
+};
+// 1.3. Imprimir sólo los números múltiplos de 20 del 1 al 1000 por
+// pantalla
+for (let i=1; i<=1000; i++){
+    if (i % 20 ===0) {
+    console.log("M20 " + i);//La alerta es un peñazo, lo cambio a console.log
+    }
+};
+// 1.4. Dibujar un cuadrado por pantalla
