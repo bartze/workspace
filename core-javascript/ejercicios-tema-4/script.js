52fc3757cd7334e0b19d3e49919049c673cd4ffe
Iñaki <inaki.barcelo@gmail.com>
2024-09-29 18:43:32 +0200

    Ejercicios 1, 2, 3, 4, 5 y 6 del Tema 4 resueltos

--- core-javascript/ejercicios-tema-4/script.js
+++ core-javascript/ejercicios-tema-4/script.js
@@ -17,3 +17,70 @@ for (let i=1; i<=1000; i++){
     }
 };
 // 1.4. Dibujar un cuadrado por pantalla
+for (i = 0; i < 10; i++) {
+    document.write("_");
+}
+document.write("<br>");
+for (i = 0; i < 4; i++) {
+    document.write("|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>");
+}
+for (i = 0; i < 17; i++) {
+    document.write("‾");
+};
+//1.5. Dibujar un rombo por pantalla
+document.write("<br>");
+const dibujarRombo = (n) => {
+    // Parte superior del rombo
+    for (let i = 1; i <= n; i++) {
+        // Espacios en blanco
+        document.write("&nbsp;".repeat(n - i));
+        // Asteriscos
+        document.write("*".repeat(i));
+        document.write("<br>");
+    }
+    // Parte inferior del rombo
+    for (let i = n - 1; i >= 1; i--) {
+        // Espacios en blanco
+        document.write("&nbsp;".repeat(n - i));
+        // Asteriscos
+        document.write("*".repeat(i));
+        document.write("<br>");
+    }
+}
+dibujarRombo(5);
+
+document.write("<br>");
+function dibujarRomboConsola(tamano) {
+    // La mitad del tamaño del rombo
+    const mitad = Math.floor(tamano / 2);
+    // Parte superior del rombo
+    for (let i = 0; i <= mitad; i++) {
+      let linea = ' '.repeat(mitad - i);
+      linea += '*'.repeat(2 * i + 1);
+      console.log(linea);
+    }
+    // Parte inferior del rombo
+    for (let i = mitad - 1; i >= 0; i--) {
+      let linea = ' '.repeat(mitad - i);
+      linea += '*'.repeat(2 * i + 1);
+      console.log(linea);
+    }
+  }
+  dibujarRomboConsola(10);
+
+//1.6. Imprimir por pantalla las tablas de multiplicar del 0 al 9
+for (let i = 0; i < 10; i++) {
+    console.log("Tabla de multiplicar del " + i);
+    for (let j = 0; j <= 10; j++) {
+        console.log(i + " x " + j + " = " + (j * i));
+    }
+};
+// 1.7. Página web ejecutada en bucle infinito que pregunta por una
+// multiplicación y te responde con la solución mientras te vuelve a
+// preguntar de nuevo. 
+
+
+
+
+
+
