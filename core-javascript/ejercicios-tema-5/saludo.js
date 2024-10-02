5f6c384e8553b055c38d1559017bddff3c6ec234
Iñaki <inaki.barcelo@gmail.com>
2024-10-02 11:11:30 +0200

    Ejercicio 2 Tema 5 completo

---
+++ core-javascript/ejercicios-tema-5/saludo.js
@@ -0,0 +1,6 @@
+// 2. Define una función que al clicar el botón del ratón la llames y te escriba
+// en la página un saludo
+const saludar = () => {
+    let saludo = 'This is the way';
+    document.getElementById('resultado').innerHTML = saludo; 
+};
