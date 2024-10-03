7fb2ff4112590311984885734b9e2c6cf9aa5846
Iñaki <inaki.barcelo@gmail.com>
2024-10-03 11:28:49 +0200

    Ejercicio 5 Tema 5 completo

--- core-javascript/ejercicios-tema-5/ejercicio-5-digito/script.js
+++ core-javascript/ejercicios-tema-5/ejercicio-5-digito/script.js
@@ -2,3 +2,14 @@
 // una función que diga el número de veces que aparece el dígito en el
 // número. 
 
+const contarDigito = () => {
+    const numero = document.getElementById('numero').value;
+    const digito = document.getElementById('digito').value;
+    
+    if (digito) {
+        const contador = numero.split(digito).length - 1;
+        document.getElementById('resultado').textContent = `El dígito ${digito} aparece ${contador} veces.`;
+    } else {
+        document.getElementById('resultado').textContent = "";
+    }
+};
