dce63e6fc18ae78671c490beb4f5c998ac27f1fd
Iñaki <inaki.barcelo@gmail.com>
2024-10-06 18:13:25 +0200

    Ejercicio 2 Tema 6 completo mocking function test

---
+++ core-javascript/ejercicios-tema-6/ejercicio-2-mocking functions-mapWithCb/script.js
@@ -0,0 +1,13 @@
+// 2. Mocking functions: mapWithCb
+// The mapwithCb function accepts an array and a callback.
+//It maps over the array with the callback, so we can expect the callback function to be called multiple times.
+
+// Your task is to write tests for mapWithCb,
+//which will involve writing a mock function (aka a spy) for the callback function.
+//The test cases have been provided for you.
+
+const mapWithCb = (array, mockCallback) => {
+	return array.map(mockCallback);
+};
+
+module.exports = { mapWithCb };
---
+++ core-javascript/ejercicios-tema-6/ejercicio-2-mocking functions-mapWithCb/test/script.test.js
@@ -0,0 +1,23 @@
+const { mapWithCb } = require('../script');
+
+describe('mapWithCb', () => {
+	it('debería llamar a la función de callback el número correcto de veces', () => {
+		const mockCallback = jest.fn();
+		const array = [1, 2, 3];
+
+		mapWithCb(array, mockCallback);
+
+		expect(mockCallback).toHaveBeenCalledTimes(3);
+	});
+
+	it('debería llamar a la función de callback con los argumentos correctos', () => {
+		const mockCallback = jest.fn();
+		const array = [1, 2, 3];
+
+		mapWithCb(array, mockCallback);
+
+		expect(mockCallback).toHaveBeenCalledWith(1, 0, array);
+		expect(mockCallback).toHaveBeenCalledWith(2, 1, array);
+		expect(mockCallback).toHaveBeenCalledWith(3, 2, array);
+	});
+});
