73856413e4cb5f3f8c2befbd583689ed1fa06010
Iñaki <inaki.barcelo@gmail.com>
2024-10-06 17:52:23 +0200

    Ejercicio 1 Tema 6 completo con TDD

--- core-javascript/ejercicios-tema-6/ejercicio-1-TDD-fizzBuzz/script.js
+++ core-javascript/ejercicios-tema-6/ejercicio-1-TDD-fizzBuzz/script.js
@@ -1,5 +1,35 @@
-const fizzBuzz = () => {
-    return 1
+// 1. TDD: fizzBuzz
+// Your task is to write a function, fizzBuzz, that accepts a number and returns a string:
+
+// 'fizz' if the number is divisible by 3;
+// 'buzz' if the number is divisible by 5;
+// 'fizzbuzz' if the number is divisible by both 3 and 5.
+// '{number}' if the number doesn't fulfil any of the above conditions.
+// As you create the function in fizzbuzz.js, you should also write tests in index.test.js. You'll need to create your own test cases as you go.
+
+// Edge cases to consider:
+
+// what should fizzbuzz(0) return?
+// what should happen if the function is passed a string?
+const fizzBuzz = (num) => {
+	if (typeof num !== 'number') {
+		throw new Error('El valor debe ser un número');
+	}
+	if (num < 1) {
+		throw new Error('El número debe ser mayor que 0');
+	}
+	if (num % 3 === 0 && num % 5 === 0) {
+		return 'FizzBuzz';
+	} else if (num % 3 === 0) {
+		return 'Fizz';
+	} else if (num % 5 === 0) {
+		return 'Buzz';
+	}
+	return num;
+};
+
+for (let i = 1; i <= 100; i++) {
+	console.log(fizzBuzz(i));
 }
 
 module.exports = { fizzBuzz };
--- core-javascript/ejercicios-tema-6/ejercicio-1-TDD-fizzBuzz/test/script.test.js
+++ core-javascript/ejercicios-tema-6/ejercicio-1-TDD-fizzBuzz/test/script.test.js
@@ -1,8 +1,34 @@
 const { fizzBuzz } = require('../script');
 
 describe('fizzBuzz', () => {
-  it('debería devolver 1', () => {
-    const fizzBuzzFunction = fizzBuzz();
-    expect(fizzBuzzFunction).toBe(1);
-  });
+	it('debería devolver 1 cuando se pasa 1', () => {
+		const result = fizzBuzz(1);
+		expect(result).toBe(1);
+	});
+	it('debería devolver un error cuando se le pasa 0 o menor que 0', () => {
+		expect(() => fizzBuzz(0)).toThrow('El número debe ser mayor que 0');
+	});
+	it('debería devolver un error cuando se le pasa un string', () => {
+		expect(() => fizzBuzz('a')).toThrow('El valor debe ser un número');
+	});
+	it('debería devolver Fizz cuando se le pasa 3', () => {
+		const result = fizzBuzz(3);
+		expect(result).toBe('Fizz');
+	});
+	it('debería devolver Buzz cuando se le pasa 5', () => {
+		const result = fizzBuzz(5);
+		expect(result).toBe('Buzz');
+	});
+	it('debería devolver Fizz cuando se le pasan multiplos de 3', () => {
+		const result = fizzBuzz(6);
+		expect(result).toBe('Fizz');
+	});
+	it('debería devolver Buzz cuando se le pasan multiplos de 5', () => {
+		const result = fizzBuzz(10);
+		expect(result).toBe('Buzz');
+	});
+	it('debería devolver FizzBuzz cuando se le pasan multiplos de 3 y de 5', () => {
+		const result = fizzBuzz(15);
+		expect(result).toBe('FizzBuzz');
+	});
 });
