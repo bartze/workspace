c3ee6ecd9ccada3dabee2b6a75624579e5ef5f95
Iñaki <inaki.barcelo@gmail.com>
2024-10-07 17:40:26 +0200

    Ejercicio 6 Tema 7 completo

---
+++ core-javascript/ejercicios-tema-7/ejercicio-6-identicos/test/script.test.js
@@ -0,0 +1,16 @@
+const { comprobarArraysIdenticos } = require('../script');
+
+test('comprueba si dos arrays son identicos', () => {
+	expect(comprobarArraysIdenticos([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).toBe(true);
+});
+test('arrays con diferentes longitudes', () => {
+	expect(comprobarArraysIdenticos([1, 2, 3], [1, 2, 3, 4])).toBe(false);
+});
+
+test('arrays con diferentes elementos', () => {
+	expect(comprobarArraysIdenticos([1, 2, 3], [1, 2, 4])).toBe(false);
+});
+
+test('arrays vacíos', () => {
+	expect(comprobarArraysIdenticos([], [])).toBe(true);
+});
