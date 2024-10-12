dafc2336203a10bef911de32864781685f2e575f
Iñaki <inaki.barcelo@gmail.com>
2024-10-12 13:19:59 +0200

    Ejercicios 1 y 2 de la página Medium Tema 8

---
+++ core-javascript/ejercicios-tema-8/ejercicio-3-medium/transformarMatrizProfunda.js
@@ -0,0 +1,23 @@
+// Descripción de la tarea: Escribe un método que devuelva una matriz profunda como [[clave, valor]]
+// Resultado esperado: ({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+// Complejidad de la tarea: 1 de 5
+// @param {Object} objeto : cualquier objeto para transformar en una matriz
+// @returns {Array} : una matriz profunda
+//
+// export const makePairs = (object) => {
+//     throw new Error (`ponga su solución aquí ${object}`);
+//   };
+//   constante datos = { a: 1, b: 2 };
+//   console.log(makePairs(datos)); // [['a', 1], ['b', 2]]
+
+const makePairs = (object) => {
+	if (typeof object !== 'object' || object === null) {
+		return object;
+	}
+	if (Array.isArray(object)) {
+		return object.map(makePairs);
+	}
+	return Object.keys(object).map((key) => [key, makePairs(object[key])]);
+};
+console.log(makePairs({ a: 1, b: 2 })); // [['a', 1], ['b', 2]]
+module.exports = { makePairs };
---
+++ core-javascript/ejercicios-tema-8/ejercicio-3-medium/verificarObjeto.js
@@ -0,0 +1,24 @@
+// Descripción de la tarea: Escribe un método que verifique que un argumento sea un objeto simple, no una matriz o nulo
+// Resultado esperado: Verdadero si el objeto es simple, falso en caso contrario.
+//    ({ a: 1 }) => verdadero,
+//    ([1, 2, 3]) => falso
+// Complejidad de la tarea: 1 de 5
+// @param element - elemento a verificar
+// @returns {boolean}
+//
+// export const isPlainObject = (element) => {
+//     throw new Error (`ponga su solución aquí ${element}`);
+//   };
+//   constante datos = { a: 1 };
+//   console.log(isPlainObject(datos)); // verdadero
+
+const isPlainObject = (element) => {
+	if (typeof element === 'object' && !Array.isArray(element) && element !== null) {
+		return true;
+	}
+	return false;
+};
+// console.log(isPlainObject({ a: 1 }));
+// console.log(isPlainObject([1, 2, 3]));
+// console.log(isPlainObject(null));
+module.exports = { isPlainObject };
