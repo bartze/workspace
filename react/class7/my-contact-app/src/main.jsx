90bba0c50cd1c3087ccf4584e34df386147ba823
Iñaki <inaki.barcelo@gmail.com>
2025-02-02 12:32:23 +0100

    Añado la app de contactos del tutorial

--- react/class7/my-contact-app/src/main.jsx
+++ react/class7/my-contact-app/src/main.jsx
@@ -2,19 +2,43 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Root from './routes/root';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Root />,
+		loader: rootLoader,
+		action: rootAction,
 		errorElement: <ErrorPage />,
 		children: [
 			{
-				path: 'contacts/:contactId',
-				element: <Contact />,
+				errorElement: <ErrorPage />,
+				children: [
+					{ index: true, element: <Index /> },
+					{
+						path: 'contacts/:contactId',
+						element: <Contact />,
+						loader: contactLoader,
+						action: contactAction,
+					},
+					{
+						path: 'contacts/:contactId/edit',
+						element: <EditContact />,
+						loader: contactLoader,
+						action: editAction,
+					},
+					{
+						path: 'contacts/:contactId/destroy',
+						action: destroyAction,
+						errorElement: <div>Oops! There was an error</div>,
+					},
+				],
 			},
 		],
 	},
--- react/class7/my-contact-app/src/routes/contact.jsx
+++ react/class7/my-contact-app/src/routes/contact.jsx
@@ -1,15 +1,35 @@
-import { Form } from 'react-router-dom';
+import { Form, useLoaderData, useFetcher } from 'react-router-dom';
+import { getContact, updateContact } from '../contacts';
 import PropTypes from 'prop-types';
 
+export async function action({ request, params }) {
+	const formData = await request.formData();
+	return updateContact(params.contactId, {
+		favorite: formData.get('favorite') === 'true',
+	});
+}
+
+export async function loader({ params }) {
+	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response('', {
+			status: 404,
+			statusText: 'Not Found',
+		});
+	}
+	return { contact };
+}
+
 export default function Contact() {
-	const contact = {
-		first: 'Your',
-		last: 'Name',
-		avatar: 'https://robohash.org/you.png?size=200x200',
-		twitter: 'your_handle',
-		notes: 'Some notes',
-		favorite: true,
-	};
+	const { contact } = useLoaderData();
+	// const contact = {
+	// 	first: 'Your',
+	// 	last: 'Name',
+	// 	avatar: 'https://robohash.org/you.png?size=200x200',
+	// 	twitter: 'your_handle',
+	// 	notes: 'Some notes',
+	// 	favorite: true,
+	// };
 
 	return (
 		<div id="contact">
@@ -19,7 +39,6 @@ export default function Contact() {
 					src={contact.avatar || `https://robohash.org/${contact.id}.png?size=200x200`}
 				/>
 			</div>
-
 			<div>
 				<h1>
 					{contact.first || contact.last ? (
@@ -64,9 +83,12 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-	const favorite = contact.favorite;
+	const fetcher = useFetcher();
+	const favorite = fetcher.formData
+		? fetcher.formData.get('favorite') === 'true'
+		: contact.favorite;
 	return (
-		<Form method="post">
+		<fetcher.Form method="post">
 			<button
 				name="favorite"
 				value={favorite ? 'false' : 'true'}
@@ -74,7 +96,7 @@ function Favorite({ contact }) {
 			>
 				{favorite ? '★' : '☆'}
 			</button>
-		</Form>
+		</fetcher.Form>
 	);
 }
 
